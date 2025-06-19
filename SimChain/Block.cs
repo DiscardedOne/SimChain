8ad2ab7bc6d6d45099a6198822849d3ec5c5bf17
DiscardedOne <123882869+DiscardedOne@users.noreply.github.com>
2025-06-19 19:10:09 +0530

    Transforming to Solution Style
    Added Solution files

---
+++ SimChain/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimChain
+{
+    public class Block
+    {
+        public Block(string data, string prevId) {
+            this.data = data;
+            this.prevId = prevId;
+            this.nonce = 0;
+        }
+
+        public string id;
+        public string data;
+        public string prevId;
+        public int nonce;
+    }
+}
